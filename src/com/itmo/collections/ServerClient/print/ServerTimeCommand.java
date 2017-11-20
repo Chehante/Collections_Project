b29741d43cc0cd78f9f128c7eea1e7216e36d5c7
trtilek <tartilek@gmail.com>
2017-11-20 17:39:45 +0300

    Socket server time done

---
+++ src/com/itmo/collections/ServerClient/print/ServerTimeCommand.java
@@ -0,0 +1,19 @@
+package com.itmo.collections.ServerClient.print;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class ServerTimeCommand implements Externalizable{
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+}
